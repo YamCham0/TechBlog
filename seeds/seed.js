cabc67dfcd2dda317af2f5a50ab23fff4cf3f981
Yamil Chamo <yajochakha@gmail.com>
2021-12-11 22:25:34 -0600

    It's alive

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,7 +5,7 @@ const userData = require('./userData.json');
 const projectData = require('./projectData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true});
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
